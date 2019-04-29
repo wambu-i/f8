9aec67a39d5db2b77b6a828381668563077acb72
Wambui Karuga <wambui@karuga.xyz>
2019-04-29 11:58:05 -0700

    Stuff

--- resources/utilities.py
+++ resources/utilities.py
@@ -172,10 +172,9 @@ def send_postback_replies(_id, txt, msg, token):
 					"elements": [
 						{
 						"title": txt,
-						"subtitle": "Stuff to come",
-
-						},
+						"subtitle": "Stuff to come"
 						"buttons": msg
+						}
 					]
 				}
 			}
