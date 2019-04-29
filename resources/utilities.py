2caa78ba76f87291b9a5b592f5b57583bfaed51d
Wambui Karuga <wambui@karuga.xyz>
2019-04-29 08:55:18 -0700

    Stuff.

--- resources/utilities.py
+++ resources/utilities.py
@@ -1,13 +1,152 @@
 import json
 import os
-path = os.path.abspath("agoa.json")
-def import_questions(files):
+
+import logging
+import requests
+import sys
+
+formatter = '[%(asctime)-15s] %(levelname)s [%(filename)s.%(funcName)s#L%(lineno)d] - %(message)s'
+
+logging.basicConfig(level = logging.DEBUG, format = formatter)
+
+# Create logger instance
+logger = logging.getLogger('api')
+quiz_path = os.path.abspath("agoa.json")
+resp_path = os.path.abspath("responses.json")
+
+assets = {
+	"quick": {
+		"usage" : ["introduction"],
+		"type" : "quick_replies"
+	},
+	"message" : {
+		"type": "text"
+	}
+}
+
+headers = {
+	'Content-Type' : 'application/json'
+}
+
+graph = "https://graph.facebook.com/v3.2/me/messages?access_token="
+__all__ = ['make_response', 'import_questions', 'find_user']
+
+_CURRENT_MODULE_ = sys.modules[__name__]
+
+def get_response():
+	responses = {}
+	print(resp_path)
+	try:
+		with open(resp_path, "r") as store:
+			responses = json.load(store)
+			store.close()
+	except (IOError, OSError):
+		return responses
+	return responses
+
+def make_response(_id, t, k, token):
+	loaded = None
+	message = None
+
+	response = get_response()
+	if response:
+		loaded = response.get(k, None)
+	else:
+		return None
+	if not loaded:
+		logger.error("Could not find specified option in provided responses.")
+		return None
+
+	handler_name = 'make_{}_replies'.format(t)
+	req = 'send_{}_replies'.format(t)
+	_type = assets[t]["type"]
+	try:
+		handler = getattr(_CURRENT_MODULE_, handler_name)
+		message = handler(loaded)
+		api_request = getattr(_CURRENT_MODULE_, req)
+		if handler_name == 'message':
+			if k == 'greeting':
+				msg = loaded.text + find_user(_id)
+				api_request(_id, msg, token)
+		else:
+			pass
+
+	except AttributeError:
+		logger.warning('Could not find handler for {}'.format(key))
+	return True
+
+def make_quick_replies(payload):
+	replies = []
+
+	for i in range(len(payload.get("options", None))):
+		reply = {}
+		reply["content_type"] = "text"
+		reply["title"] = payload["options"][i]
+		reply["payload"] = payload["payload"][i]
+		print(reply)
+		replies.append(reply)
+
+	return replies
+
+
+def make_message_replies(text):
+	payload = {
+		"message": text
+	}
+
+	return payload
+
+
+def send_message_replies(_id, text, token, **kwargs):
+	data = json.dumps({
+		"recipient":{
+			"id": _id
+		},
+		"message": {
+			"text": txt,
+			"quick_replies": msg
+		}
+		})
+	r = requests.post(graph.format(token), headers = headers, data = data)
+	if r.status_code == 200:
+		logger.info("Successfully made quick responses request!")
+	else:
+		logger.error('{}' - '{}'.format(r.status_code, r.text))
+
+
+
+def send_quick_replies(_id, txt, token, **kwargs):
+	data = json.dumps({
+		"recipient":{
+			"id": _id
+		},
+		"message": {
+			"text": txt,
+			"quick_replies": msg
+		}
+		})
+	r = requests.post(graph.format(token), headers = headers, data = data)
+	if r.status_code == 200:
+		logger.info("Successfully made quick responses request!")
+	else:
+		logger.error('{}' - '{}'.format(r.status_code, r.text))
+
+def import_questions():
     questions = []
     try:
-        with open(path, "r") as store:
+        with open(quiz_path, "r") as store:
             questions = json.load(store)
             store.close()
     except (IOError, OSError):
         return None
 
     return questions
+
+
+def find_user(id):
+    headers = {
+    'Content-Type' : 'application/json'
+    }
+    r = requests.get('https://graph.facebook.com/v2.10/' + id + '?fields=first_name,last_name&access_token=' + PAT, headers = headers)
+    nm = r.json()
+    return nm['first_name']
